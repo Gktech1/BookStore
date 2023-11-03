2548c712e71713527a10c9a876a46d6f8bc47773
Olukunle Edun <favourblessing1@gmail.com>
2023-11-03 19:55:33 +0100

    features updates on Order Service and Gateway

---
+++ src/Services/APIGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Cache.CacheManager;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+// Add services to the container.
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.WebHost.ConfigureLogging(loggingBuilder =>
+{
+    loggingBuilder.AddConfiguration(builder.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
